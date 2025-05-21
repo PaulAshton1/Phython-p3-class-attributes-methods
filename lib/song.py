ccc6942d88ef5c7c66a8dd3b5398e2e888a04b23
PaulAshton1 <ashtonvilla42@gmail.com>
2025-05-21 23:18:25 +0300

    new updates

--- lib/song.py
+++ lib/song.py
@@ -1,25 +1,54 @@
 class Song:
     count = 0
     artists = []
-    genre = []
+    genres = []
     genre_count = {}
     artist_count = {}
 
-    def __init__(self, artist, genre):
+    def __init__(self, name, artist, genre):
+        self.name = name
         self.artist = artist
         self.genre = genre
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        
 
+    # Tracking song count
 
     @classmethod
     def add_song_to_count(cls):
         cls.count += 1
 
-
+    # Add a new genre to the genre list
     @classmethod
-    def add_genre(cls, genre):
+    def add_to_genres(cls, genre):
         if genre not in cls.genres:
             cls.genres.append(genre)
-            cls.genre_count[genre] = 1
-        else:
-            cls.genre_count[genre] += 1    
+        cls.add_to_genre_count(genre)
 
+    # Add a new artist to the artist list
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+        cls.add_to_artist_count(artist)
+
+    # Add a genre and the count of songs in that genre
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    # Add an artist and the count of songs by that artist
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
